b6c41b1c7666997ff69ad9ebca851c853406d70d
superjoy0502 <superjoy0502@gmail.com>
2020-06-09 21:07:07 -0600

    FarmWorld.kt 주석 추가

--- src/main/kotlin/com/github/noonmaru/farm/FarmWorld.kt
+++ src/main/kotlin/com/github/noonmaru/farm/FarmWorld.kt
@@ -2,17 +2,17 @@ package com.github.noonmaru.farm
 
 import org.bukkit.World
 
-interface FarmWorld {
-    val name: String
-    val bukkitWorld: World
-    val loadedChunks: List<FarmChunk>
+interface FarmWorld { // FarmWorld ==> 서버 월드들을 해당 플러그인에서 관리하기 위한 인터페이스
+    val name: String // 월드 이름 (String)
+    val bukkitWorld: World // 월드 (org.bukkit.World)
+    val loadedChunks: List<FarmChunk> // 로딩된 청크 리스트 (List<FarmChunk>)
 
-    fun chunkAt(x: Int, z: Int): FarmChunk?
+    fun chunkAt(x: Int, z: Int): FarmChunk? // x, z 좌표를 입력받고 해당 위치의 팜청크를 반환; null값이 반환될 수 있음
 
-    fun cropAt(x: Int, y: Int, z: Int): FarmCrop?
+    fun cropAt(x: Int, y: Int, z: Int): FarmCrop? // x, y, z 좌표를 입력받고 해당 위치에 있는 작물을 반환; null값이 반환될 수 있음
 }
 
-val World.farmWorld: FarmWorld
-    get() {
-        return Farm.manager.getWorld(this)
+val World.farmWorld: FarmWorld // World.farmWorld 변수 생성; org.bukkit.World 클래스 안의 변수; 다른 절차 없이 World 클래스를 이용해 호출 할 수 있음
+    get() { // World.farmWorld를 불러올 때
+        return Farm.manager.getWorld(this) // Farm 오브젝트(FarmManager 클래스의 하위 클래스라고 해석하면 편함)의 manager를 통해 FarmManager.getWorld()를 호출 및 반환
     }
